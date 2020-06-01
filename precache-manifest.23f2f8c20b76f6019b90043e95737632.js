d171fac4a26825c34c988f74f333786a1f29d6cc
William Wong <wwongqwerty@gmail.com>
2020-06-01 15:46:18 -0400

    Updates

---
+++ precache-manifest.23f2f8c20b76f6019b90043e95737632.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e6f66fae62f098005d5fd6459b987d1",
+    "url": "/sulis/index.html"
+  },
+  {
+    "revision": "52d8d9d266ceea299c4e",
+    "url": "/sulis/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "9d43db1cd71ac00f2ce3",
+    "url": "/sulis/static/css/main.7384647c.chunk.css"
+  },
+  {
+    "revision": "52d8d9d266ceea299c4e",
+    "url": "/sulis/static/js/2.e5aac4c0.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/sulis/static/js/2.e5aac4c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d43db1cd71ac00f2ce3",
+    "url": "/sulis/static/js/main.916f163a.chunk.js"
+  },
+  {
+    "revision": "f411f2567ee06ce0b412",
+    "url": "/sulis/static/js/runtime-main.0b9282b1.js"
+  },
+  {
+    "revision": "08f9369e8b41a0705192f321a189afb8",
+    "url": "/sulis/static/media/anurag.08f9369e.png"
+  },
+  {
+    "revision": "084877b7fb13570639f33a4845eca3d9",
+    "url": "/sulis/static/media/ari.084877b7.png"
+  },
+  {
+    "revision": "ca58d0ed379670e5899fddbb136b5b90",
+    "url": "/sulis/static/media/cover.ca58d0ed.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sulis/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "1e294a8a552c497f32e4104cb5a15d77",
+    "url": "/sulis/static/media/sarah.1e294a8a.jpg"
+  },
+  {
+    "revision": "adebdd538d975d4548d7493498b1af0b",
+    "url": "/sulis/static/media/sulisbanner.adebdd53.png"
+  },
+  {
+    "revision": "b4c5a71ed50f0570d293968f1bb26810",
+    "url": "/sulis/static/media/yuki.b4c5a71e.png"
+  }
+]);
